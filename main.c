a79d1e9f74c7ac316b32cda984cb558fd3b039be
hazcoper <hazcoper@pop-os.localdomain>
2020-10-08 09:53:18 +0100

    added color

--- main.c
+++ main.c
@@ -16,15 +16,22 @@ int kbhit(void){
 int main(void){
     initscr();
 
+    start_color();
+    init_pair(1,COLOR_RED,COLOR_BLUE);
+    attron(COLOR_PAIR(1));
     cbreak();
     noecho();
     nodelay(stdscr, TRUE);
 
+    printw("word");
+
     scrollok(stdscr, TRUE);
+    init_pair(1, COLOR_BLACK, COLOR_RED);
     while (1) {
         if (kbhit()) {
             printw("%c", getch());
             refresh();
         }
     }
+    attroff(COLOR_PAIR(1));
 }
